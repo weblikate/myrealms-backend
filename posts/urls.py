d4b1900990f7f9f3573b2906aa525a94c91187f7
rishav4101 <rajkumarrishav4101@gmail.com>
2020-08-23 00:40:00 +0530

    model view set

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from posts import views
 
-urlpatterns = [
-    path('WritePosts/', views.WritePostsList.as_view()),
-    path('WritePosts/<int:pk>/', views.WritePostsDetail.as_view()),
-    path('ClickPosts/', views.ClickPostsList.as_view()),
-    path('ClickPosts/<int:pk>/', views.ClickPostsDetail.as_view()),
-    path('ArtPosts/', views.ArtPostsList.as_view()),
-    path('ArtPosts/<int:pk>/', views.ArtPostsDetail.as_view()),
+router = DefaultRouter()
+router.register(r'writeposts', views.WritePostsViewSet)
+router.register(r'clickposts', views.ClickPostsViewSet)
+router.register(r'artposts', views.ArtPostsViewSet)
 
+urlpatterns = [
+    # path('WritePosts/', views.WritePostsList.as_view()),
+    # path('WritePosts/<int:pk>/', views.WritePostsDetail.as_view()),
+    # path('ClickPosts/', views.ClickPostsList.as_view()),
+    # path('ClickPosts/<int:pk>/', views.ClickPostsDetail.as_view()),
+    # path('ArtPosts/', views.ArtPostsList.as_view()),
+    # path('ArtPosts/<int:pk>/', views.ArtPostsDetail.as_view()),
+    path('', include(router.urls)),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- posts/views.py
+++ posts/views.py
@@ -1,41 +1,57 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from posts.models import *
 from posts.serializers import *
 from rest_framework.permissions import IsAuthenticated,DjangoModelPermissions,AllowAny
 # Create your views here.
 
-class WritePostsList(generics.ListCreateAPIView):
+# class WritePostsList(generics.ListCreateAPIView):
+#     queryset = WritePosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = WritePostsSerializer
+
+
+# class WritePostsDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = WritePosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = WritePostsSerializer
+
+
+# class ClickPostsList(generics.ListCreateAPIView):
+#     queryset = ClickPosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = ClickPostsSerializer
+
+
+# class ClickPostsDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = ClickPosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = ClickPostsSerializer
+
+
+# class ArtPostsList(generics.ListCreateAPIView):
+#     queryset = ArtPosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = ArtPostsSerializer
+
+
+# class ArtPostsDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = ArtPosts.objects.all()
+#     permission_classes=(IsAuthenticated,)
+#     serializer_class = ArtPostsSerializer
+
+
+class WritePostsViewSet(viewsets.ModelViewSet):
     queryset = WritePosts.objects.all()
-    permission_classes=(IsAuthenticated,)
+    permission_classes= [IsAuthenticated,]
     serializer_class = WritePostsSerializer
 
-
-class WritePostsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = WritePosts.objects.all()
-    permission_classes=(IsAuthenticated,)
-    serializer_class = WritePostsSerializer
-
-
-class ClickPostsList(generics.ListCreateAPIView):
+class ClickPostsViewSet(viewsets.ModelViewSet):
     queryset = ClickPosts.objects.all()
-    permission_classes=(IsAuthenticated,)
+    permission_classes= [IsAuthenticated,]
     serializer_class = ClickPostsSerializer
 
-
-class ClickPostsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ClickPosts.objects.all()
-    permission_classes=(IsAuthenticated,)
-    serializer_class = ClickPostsSerializer
-
-
-class ArtPostsList(generics.ListCreateAPIView):
+class ArtPostsViewSet(viewsets.ModelViewSet):
     queryset = ArtPosts.objects.all()
-    permission_classes=(IsAuthenticated,)
-    serializer_class = ArtPostsSerializer
-
-
-class ArtPostsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ArtPosts.objects.all()
-    permission_classes=(IsAuthenticated,)
+    permission_classes= [IsAuthenticated,]
     serializer_class = ArtPostsSerializer
